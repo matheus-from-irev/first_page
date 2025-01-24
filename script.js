2962bf482578ec24f1e3728529df1a613afdb0c5
matheus-from-irev <matheus.m@irev.com>
2025-01-24 16:35:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("redirect-button").addEventListener("click", function() 
 
   // INSERT POSTBACK URL OF THE GOAL TYPE HERE
   var postback = "https://demo-igaming.irev.com/backend/api/v3/goal-type-fire/16?hash=REPLACE"
-  var redirectionPage = "https://secondpage.pages.dev/"
+  var redirectionPage = "https://second-page-rho.vercel.app/"
 
   const clickId = new URLSearchParams(window.location.search).get("click_id");
   postback = postback.replace("REPLACE", clickId);
